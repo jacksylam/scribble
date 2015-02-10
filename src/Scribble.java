2bdea7944b33bdbdb842420a74d6944109039c41
Jack Lam <catinacan@gmail.com>
2015-02-10 00:06:31 -1000

    Adhere to code standards.

--- src/Scribble.java
+++ src/Scribble.java
@@ -1,53 +1,101 @@
-// This example is from _Java Examples in a Nutshell_. (http://www.oreilly.com)
-// Copyright (c) 1997 by David Flanagan
-// This example is provided WITHOUT ANY WARRANTY either expressed or implied.
-// You may study, use, modify, and distribute it for non-commercial purposes.
-// For any commercial use, see http://www.davidflanagan.com/javaexamples
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Event;
+import java.awt.Graphics;
 
-import java.applet.*;
-import java.awt.*;
+/**
+ * This example is from _Java Examples in a Nutshell_. (http://www.oreilly.com)
+ * Copyright (c) 1997 by David Flanagan
+ * This example is provided WITHOUT ANY WARRANTY either expressed or implied.
+ * You may study, use, modify, and distribute it for non-commercial purposes.
+ * For any commercial use, see http://www.davidflanagan.com/javaexamples
+ */
 
-/** A simple applet that uses the Java 1.0 event handling model */
+/**
+ * A simple applet that uses the Java 1.0 event handling model.
+ */
 public class Scribble extends Applet {
   private int lastx, lasty;    // remember last mouse coordinates
-  Button clear_button;         // the Clear button
+  Button clearButton;         // the Clear button
   Graphics g;                  // A Graphics object for drawing
 
-  /** Initialize the button and the Graphics object */
+  /**
+   * Initialize the button and the Graphics object.
+   */
   public void init() {
-    clear_button = new Button("Clear");
-    this.add(clear_button);
+    clearButton = new Button("Clear");
+    this.add(clearButton);
     g = this.getGraphics();
   }
-  /** Respond to mouse clicks */
+
+  /**
+   * Respond to mouse clicks.
+   *
+   * @param e The event happening.
+   * @param x Current x coordinates.
+   * @param y Current y coordinates.
+   * @return true for successful reassignment.
+   */
   public boolean mouseDown(Event e, int x, int y) {
-    lastx = x; lasty = y;
+    lastx = x;
+    lasty = y;
     return true;
   }
-  /** Respond to mouse drags */
+
+  /**
+   * Respond to mouse drags.
+   *
+   * @param e The event happening.
+   * @param x Current x coordinates.
+   * @param y Current y coordinates.
+   * @return true for successful reassignment.
+   */
   public boolean mouseDrag(Event e, int x, int y) {
     g.setColor(Color.black);
     g.drawLine(lastx, lasty, x, y);
-    lastx = x; lasty = y;
+    lastx = x;
+    lasty = y;
     return true;
   }
-  /** Respond to key presses */
+
+  /**
+   * Respond to key presses.
+   *
+   * @param e   The event happening.
+   * @param key The key.
+   * @return True if key was "c", false otherwise.
+   */
   public boolean keyDown(Event e, int key) {
     if ((e.id == Event.KEY_PRESS) && (key == 'c')) {
       clear();
       return true;
     }
-    else return false;
+    else {
+      return false;
+    }
   }
-  /** Respond to Button clicks */
+
+  /**
+   * Respond to Button clicks.
+   *
+   * @param e   The event happening.
+   * @param arg Object.
+   * @return True if target is button, false otherwise.
+   */
   public boolean action(Event e, Object arg) {
-    if (e.target == clear_button) {
+    if (e.target == clearButton) {
       clear();
       return true;
     }
-    else return false;
+    else {
+      return false;
+    }
   }
-  /** convenience method to erase the scribble */
+
+  /**
+   * Convenience method to erase the scribble.
+   */
   public void clear() {
     g.setColor(this.getBackground());
     g.fillRect(0, 0, bounds().width, bounds().height);
---
+++ src/package-info.java
@@ -0,0 +1,3 @@
+/**
+ * Created by Jack on 2/9/2015.
+ */
